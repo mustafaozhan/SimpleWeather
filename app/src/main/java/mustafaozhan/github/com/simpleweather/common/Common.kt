b85af32c82a842c2425c35cacb663f03032f6920
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-09-30 11:33:56 +0300

    Common package and object implemented

---
+++ app/src/main/java/mustafaozhan/github/com/simpleweather/common/Common.kt
@@ -0,0 +1,34 @@
+package mustafaozhan.github.com.simpleweather.common
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Mustafa Ozhan on 9/30/17 at 11:11 AM on Arch Linux.
+ */
+object Common {
+    private val API_KEY = "365891e5ccaa5c76adac3f14ce4f2a26"
+    private val API_LINK = "http://api.openweathermap.org/data/2.5"
+
+
+    fun apiRequest(lat: String, lng: String): String {
+        val sb = StringBuilder(API_LINK)
+        sb.append("?lat=$lat&lon=$lng&APPID=$API_KEY&units=metric")
+        return sb.toString()
+    }
+
+    fun unixTimeStampToDateTime(unixTimeStamp: Double): String {
+        val dateFormat = SimpleDateFormat("HH:mm")
+        val date = Date()
+        date.time = unixTimeStamp.toLong() * 1000
+        return dateFormat.format(date)
+    }
+
+    fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("dd MM yyyy HH:mm")
+        val date = Date()
+        return dateFormat.format(date)
+    }
+
+    fun getImage(icon: String) = "http://openweathermap.org/img/w/$icon.png"
+}
